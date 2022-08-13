39c3dc68be40b5a7a414a165680568a612fdd9fb
Marisbel Konrad <marisbel.konrad@cwi.com.br>
2022-08-13 14:59:21 -0300

    add base project

---
+++ src/main/java/br/com/cwi/hellodocker/HellodockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.cwi.hellodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellodockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellodockerApplication.class, args);
+	}
+
+}
